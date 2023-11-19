50a11b52bebbdcfad5fbaecd13177da695a0bdd2
Ruben <rubencidep@gmail.com>
2023-11-19 21:00:21 +0100

    Aplicación TODO actualizada

--- app/src/main/java/com/example/myapplicationpruebas/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/myapplicationpruebas/todoapp/TodoActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplicationpruebas.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
     private lateinit var rvCategories :RecyclerView
     private lateinit var categoryAdapter: CategoriesAdapter
     private lateinit var rvTasks:RecyclerView
     private lateinit var taskAdapter: TasksAdapter
+    private lateinit var fabAddTask :FloatingActionButton
 
     private val categories = listOf(
         TaskCategory.Personal,
@@ -27,12 +29,13 @@ class TodoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
+        iniTListener()
     }
-
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories )
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
     private fun initUI() {
         categoryAdapter = CategoriesAdapter(categories)
@@ -42,6 +45,14 @@ class TodoActivity : AppCompatActivity() {
         taskAdapter = TasksAdapter(tasks)
         rvTasks.layoutManager= LinearLayoutManager(this)
         rvTasks.adapter = taskAdapter
+    }
+    private fun iniTListener() {
+        fabAddTask.setOnClickListener {
+
+        }
+    }
+    private fun showDialog(){
+
     }
 
 }
